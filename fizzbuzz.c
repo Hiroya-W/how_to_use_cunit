5f1f964b076e630b8103522be2d10511cdc8b858
Hiroya-W <b8122508@edu.kit.ac.jp>
2020-11-26 00:46:52 +0900

    :+1: fizzbuzzに機能を追加した

--- fizzbuzz.c
+++ fizzbuzz.c
@@ -1,13 +1,33 @@
 #include <stdio.h>
 
+static const char *get_str_FizzBuzz(void);
+static const char *get_str_Fizz(void);
+static const char *get_str_Buzz(void);
+
+static const char *STR_FIZZBUZZ = "FizzBuzz";
+static const char *STR_FIZZ = "Fizz";
+static const char *STR_BUZZ = "Buzz";
+
 void fizzbuzz(int num, char *result) {
     if ((num % 3 == 0) && (num % 5 == 0)) {
-        sprintf(result, "FizzBuzz");
+        sprintf(result, get_str_FizzBuzz());
     } else if (num % 3 == 0) {
-        sprintf(result, "Fizz");
+        sprintf(result, get_str_Fizz());
     } else if (num % 5 == 0) {
-        sprintf(result, "Buzz");
+        sprintf(result, get_str_Buzz());
     } else {
         sprintf(result, "%d", num);
     }
+}
+
+static const char *get_str_FizzBuzz(void) {
+    return STR_FIZZBUZZ;
+}
+
+static const char *get_str_Fizz(void) {
+    return STR_FIZZ;
+}
+
+static const char *get_str_Buzz(void) {
+    return STR_BUZZ;
 }
